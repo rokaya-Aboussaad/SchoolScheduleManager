343af0e09fd371896af42c32890c992c4ea28485
rokaya-Aboussaad <121617948+rokaya-Aboussaad@users.noreply.github.com>
2026-03-05 16:01:32 +0000

    Delete JavaProject-gestionEmploiTemps directory

--- JavaProject-gestionEmploiTemps/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.essprjtjava {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-    opens com.essprjtjava to javafx.fxml;
-    opens com.essprjtjava.controller to javafx.fxml;
-    exports com.essprjtjava;
-}
